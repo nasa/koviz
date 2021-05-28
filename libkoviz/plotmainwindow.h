fbdfb4f2eaf15f22bdbeaa1c7223aaeba13488ca
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-28 18:17:31 -0300

    Update plotmainwindow.h

--- libkoviz/plotmainwindow.h
+++ libkoviz/plotmainwindow.h
@@ -1,187 +1,186 @@
-#ifndef PLOTMAINWINDOW_H
-#define PLOTMAINWINDOW_H
+# PLOTMAINWINDOW_H
+# PLOTMAINWINDOW_H
 
-#include <QMainWindow>
-#include <QMenu>
-#include <QMenuBar>
-#include <QFrame>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QGridLayout>
-#include <QSplitter>
-#include <QList>
-#include <QHash>
-#include <QStandardItemModel>
-#include <QFileInfo>
-#include <QStringList>
-#include <QFileDialog>
-#include <QMessageBox>
-#include <QLineEdit>
-#include <QSizePolicy>
-#include <QTreeView>
-#include <QFile>
-#include <QTextStream>
-#include <QDate>
-#include <QSettings>
-#include <QProcess>
-#include <QTcpSocket>
-#include <QStatusBar>
+# <QMainWindow>
+# <QMenu>
+# <QMenuBar>
+# <QFrame>
+# <QVBoxLayout>
+# <QHBoxLayout>
+# <QGridLayout>
+# <QSplitter>
+# <QList>
+# <QHash>
+# <QStandardItemModel>
+# <QFileInfo>
+# <QStringList>
+# <QFileDialog>
+# <QMessageBox>
+# <QLineEdit>
+# <QSizePolicy>
+# <QTreeView>
+# <QFile>
+# <QTextStream>
+# <QDate>
+# <QSettings>
+# <QProcess>
+# <QTcpSocket>
+# <QStatusBar>
 
-#include "monte.h"
-#include "dp.h"
-#include "bookmodel.h"
-#include "dptreewidget.h"
-#include "varswidget.h"
-#include "monteinputsview.h"
-#include "timeinput.h"
-#include "bookview.h"
-#include "runs.h"
-#include "timecom.h"
-#include "videowindow.h"
+# "monte.h"
+# "dp.h"
+# "bookmodel.h"
+# "dptreewidget.h"
+# "varswidget.h"
+# "monteinputsview.h"
+# "timeinput.h"
+# "bookview.h"
+# "runs.h"
+# "timecom.h"
+# "videowindow.h"
 
-class PlotMainWindow : public QMainWindow
+ PlotMainWindow :  QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit PlotMainWindow( PlotBookModel* bookModel,
-                             const QString& trickhost,
-                             uint trickport,
-                             double trickoffset,
-                             const QString& videoFileName,
-                             double videoOffset,
-                             const QString& excludePattern,
-                             const QString& filterPattern,
-                             const QString& scripts,
-                             bool isDebug,
-                             bool isPlotAllVars,
-                             const QStringList& timeNames,
-                             const QString& dpDir,
-                             const QStringList& dpFiles,
-                             bool isShowTables,
+     PlotMainWindow( PlotBookModel* bookModel,
+                              QString& trickhost,
+                              trickport,
+                             trickoffset,
+                              QString& videoFileName,
+                              videoOffset,
+                              QString& excludePattern,
+                              QString& filterPattern,
+                              QString& scripts,
+                              isDebug,
+                              isPlotAllVars,
+                              QStringList& timeNames,
+                              QString& dpDir,
+                              QStringList& dpFiles,
+                              isShowTables,
                              QStringList unitOverrides,
                              QString map,
                              QString mapFile,
-                             Runs* runs,
-                             QStandardItemModel* varsModel,
-                             QStandardItemModel* monteInputsModel=0,
-                             QWidget *parent = 0);
+                             Run runs,
+                             QStandardItemModel varsModel,
+                             QStandardItemModel monteInputsModel=0,
+                             QWidget *parent 0);
 
-     void savePdf(const QString& fname);
+      savePdf( QStrin fname);
 
     ~PlotMainWindow();
 
 protected:
-     virtual void closeEvent(QCloseEvent *event);
+     virtual closeEvent(QCloseEvent *);
 
 
 private:
     PlotBookModel* _bookModel;
 
-    const QString& _trickhost;
-    uint _trickport;
-    double _trickoffset;
+     QString& _trickhost;
+   uint_  _trickport;
+     _trickoffset;
     QString _videoFileName;
-    double _videoOffset;
+     _videoOffset;
     QString _excludePattern;
     QString _filterPattern;
     QString _scripts;
-    bool _isDebug;
+     _isDebug;
     QStringList _timeNames;
     QString _dpDir;
     QStringList _dpFiles;
-    bool _isShowTables;
+     _isShowTables;
     QStringList _unitOverrides;
     QString _map;
     QString _mapFile;
-    Runs* _runs;
+    Run _runs;
     QStandardItemModel* _varsModel;
     QStandardItemModel* _monteInputsModel;
     MonteInputsView* _monteInputsView;
     QHeaderView* _monteInputsHeaderView;
     TimeInput* _timeInput;
 
-    QGridLayout* _layout;
-    QGridLayout* _left_lay ;
+    QGridLayout _layout;
+    QGridLayout _left_lay ;
 
-    void createMenu();
-    QMenuBar* _menuBar;
-    QMenu *_fileMenu;
-    QMenu *_optsMenu;
-    QMenu *_scriptsMenu;
-    QAction *_dpAction;
-    QAction *_pdfAction;
-    QAction *_jpgAction;
-    QAction *_sessionAction;
-    QAction *_openVideoAction;
-    QAction *_exitAction;
-    QAction *_showLiveCoordAction;
-    QAction *_clearPlotsAction;
-    QAction *_clearTablesAction;
-    QAction *_plotAllVarsAction;
+     createMenu();
+    QMenuBar menuBar;
+    QMenu fileMenu;
+    QMenu optsMenu;
+    QMenu scriptsMenu;
+    QAction dpAction;
+    QAction pdfAction;
+    QAction jpgAction;
+    QAction sessionAction;
+    QAction openVideoAction;
+    QAction exitAction;
+    QAction showLiveCoordAction;
+    QAction clearPlotsAction;
+    QAction clearTablesAction;
+    QAction plotAllVarsAction;
 
-    QTabWidget* _nbDPVars;
-    VarsWidget* _varsWidget;
-    QFrame* _dpFrame ;
-    DPTreeWidget* _dpTreeWidget;
+    QTabWidget nbDPVars;
+    VarsWidget varsWidget;
+    QFrame _dpFrame ;
+    DPTreeWidget _dpTreeWidget;
 
-    QTreeView* _plotTreeView ;
-    BookView* _bookView;
+    QTreeView _plotTreeView ;
+    BookView _bookView;
 
-    QStatusBar* _statusBar;
+    QStatusBar _statusBar;
 
-    bool _isRUN(const QString& fp);
-    bool _isMONTE(const QString& fp);
+     RUN(QString& fp);
+    MONTE(QString& fp);
 
-    void _readSettings();
-    void _writeSettings();
-    void _readMainWindowSettings();
-    void _readVideoWindowSettings();
+     _readSettings();
+     _writeSettings();
+     _readMainWindowSettings();
+     _readVideoWindowSettings();
 
     QModelIndex _currCurveIdx();
 
-    TimeCom* _the_visualizer;
-    TimeCom* _blender;
+    TimeCom _the_visualizer;
+    TimeCom _blender;
 
-    VideoWindow* vidView;
-    QTcpSocket* _vsSocket ;
+    VideoWindow vidView;
+    QTcpSocket _vsSocket ;
 
-    void _openVideoFile(const QString& fname);
+     _openVideoFile( QString);
 
 
-private slots:
-     void _nbCurrentChanged(int i);
-     void _bookModelRowsAboutToBeRemoved(const QModelIndex& pidx,
-                                         int start, int end);
-     void _saveDP();
-     void _savePdf();
-     void _saveJpg();
-     void _saveSession();
-     void _openVideo();
-     void _toggleShowLiveCoord();
-     void _clearPlots();
-     void _clearTables();
-     void _launchScript(QAction *action);
-     void _plotAllVars();
+ slots:
+      _nbCurrentChanged(int i);
+      _bookModelRowsAboutToBeRemoved( QModelIndex& pidx,
+                                          start, end);
+      _saveDP();
+      _savePdf();
+      _saveJpg();
+      _saveSession();
+      _openVideo();
+      _toggleShowLiveCoord();
+      _clearPlots();
+      _clearTables();
+      _launchScript(QAction *action);
+      _plotAllVars();
 
-     void _startTimeChanged(double startTime);
-     void _liveTimeChanged(double liveTime);
-     void _stopTimeChanged(double stopTime);
-     void _liveTimeNext();
-     void _liveTimePrev();
-     void _monteInputsHeaderViewClicked(int section);
-     void _monteInputsViewCurrentChanged(const QModelIndex& currIdx,
+      _startTimeChanged(double startTime);
+      _liveTimeChanged(double liveTime);
+      _stopTimeChanged(double stopTime);
+      _liveTimeNext();
+      _liveTimePrev();
+      _monteInputsHeaderViewClicked(section);
+      _monteInputsViewCurrentChanged(QModelIndex& currIdx,
                                          const QModelIndex& prevIdx);
-     void _bookViewCurrentChanged(const QModelIndex& currIdx,
-                                  const QModelIndex& prevIdx);
-     void _bookModelDataChanged(const QModelIndex &topLeft,
-                                const QModelIndex &bottomRight,
-                                const QVector<int> &roles = QVector<int>());
+      _bookViewCurrentChanged( QModelIndex& currIdx,
+                                   QModelIndex& prevIdx);
+      _bookModelDataChanged( QModelIndex &topLeft,
+                                 QModelIndex &bottomRight,
+                                 QVectorint &roles  QVectorint());
 
-     void setTimeFromVideo(double time);
-     void setTimeFromBvis(double time);
-     void _scriptError(QProcess::ProcessError error);
-     void _vsRead();
+      setTimeFromVideo(double time);
+      setTimeFromBvis(double time);
+      _scriptError(QProcess::ProcessError ();
+      _vsRead();
 };
 
-#endif // PLOTMAINWINDOW_H
