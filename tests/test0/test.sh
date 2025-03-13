0212dc50596099e6ecd65332286027be58cfd259
Keith Vetter <keith.vetter@gmail.com>
2025-03-13 06:54:52 -0500

    Test0 was not cleaning up its pdf test output

--- tests/test0/test.sh
+++ tests/test0/test.sh
@@ -21,15 +21,18 @@ koviz DP_logx a.csv
 echo "Expect logx to be Empty (PDF)"
 ~/dev/koviz/bin/koviz DP_logx a.csv -pdf moo.pdf
 evince moo.pdf
+rm moo.pdf
 
 echo "Expect logy to be Empty"
 koviz DP_logy a.csv 
 echo "Expect logy to be Empty (PDF)"
 koviz DP_logy a.csv -pdf moo.pdf
 evince moo.pdf
+rm moo.pdf
 
 echo "Expect logxy to be Empty"
 koviz DP_logxy a.csv 
 echo "Expect logxy to be Empty (PDF)"
 koviz DP_logxy a.csv -pdf moo.pdf
 evince moo.pdf
+rm moo.pdf
