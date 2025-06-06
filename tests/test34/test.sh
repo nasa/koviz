d541d1b54df8e3792ae9951f8c9e99682f0b78b1
Keith Vetter <keith.vetter@gmail.com>
2025-06-06 14:11:06 -0500

    Add time match tolerance to curve combo test

--- tests/test34/test.sh
+++ tests/test34/test.sh
@@ -3,6 +3,9 @@
 echo "---------------------------------------------------"
 echo "              Test Envelope                        "
 echo "---------------------------------------------------"
+echo "# Note: some of the csv data is purposefully off by"
+echo "# 0.01 to force -tmt 0.1 (time match tolerance) to "
+echo "# align points"
 echo ""
 echo "Action1: Press 's' for sum"
 echo "Expect1: Sum=[(0,7),(1,7),(2,2),(3,12),(4,9),(4.5,9),
@@ -18,4 +21,4 @@ echo ""
 echo "         lower_bound=[(0,1),(1,2),(2,1),(3,3),(4,2),"
 echo "                      (4.5,2*),(5,1),(6,0),(7,3),(8,1),(9,10),"
 echo "                      (10,2),(11,3),(12,4)]"
-koviz *.csv -a
+koviz *.csv -a -tmt 0.1
