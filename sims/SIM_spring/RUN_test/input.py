97b22e70501912b79f555952092b5afc961734ca
Keith Vetter <keith.vetter@gmail.com>
2025-10-20 08:57:40 -0500

    Introduce spring analytic solution
    Derived and coded an analytic solution
    to the spring for comparing different
    integrators.
    
    This commit also makes the MC spring
    optional instead of the default mode
    of operation.

--- sims/SIM_spring/RUN_test/input.py
+++ sims/SIM_spring/RUN_test/input.py
@@ -7,9 +7,12 @@ drg0.set_cycle(0.01)
 drg0.add_variable("spring.pos", "spring.pos")
 drg0.add_variable("spring.vel", "spring.vel")
 drg0.add_variable("spring.acc", "spring.acc")
+drg0.add_variable("spring.analytic_pos")
+drg0.add_variable("spring.analytic_vel")
+drg0.add_variable("spring.analytic_acc")
 trick.add_data_record_group( drg0 )
 
-trick.mc_set_enabled(1)
+trick.mc_set_enabled(0)
 trick.mc_set_num_runs(100)
 
 var0 = trick.MonteVarFile("spring.zeta", "monte_file.txt", 2, "--")
